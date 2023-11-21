e8c3a23c684372a11f3c4df11de25c334266c881
ImanBanerjee <imanbanerjee03@gmail.com>
2023-11-21 16:48:58 +0530

    Learned new things :)

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-print("hello world")
+# "Shape of you" - I don't know the singer, lazy to search
+print("The club isn't the best place to find a lover")
+print("So the bar is where I go")
+print("Me and my friends at the table doing shots")
+print()
+print("Drinking fast and then we talk slow")
+print("And you come over and start up a conversation with just me And trust me I'll give it a chance now")
+
+# Variables in python
+
+name = "Iman banerjee"
+# Variables should in lower case but can be in upper case as per my knowledge till now
+
+roll = 12
+standard = 10
+section = "N"
+
+print("Hello my name is", name, "my education", roll, standard, section)
+
+print(roll + standard)
+
+print(name * roll)  # prints "name" variable the times of roll variable
+
+# + plus
+# - minus
+# / divide
+# * times
+# ** exponentiation
+# % modulo
+# < less than
+# > greater than
+
+print("What is the answer to life, the universe, and everything?", int((40 + 30 - 7) * 2 / 3))
+# () is important here also :)
+
+print("Is it true that 5 * 2 > 3 * 4?")
+
+print(5 * 2 > 3 * 4)
+
+print("What is 5 * 2?", 5 * 2)
+print("What is 3 * 4?", 3 * 4)
+
+
+print ("Is it true that 5 * 2 > 3 * 4?", 5 * 2 > 4)
+
+# for int run int, for float run float
+
+print ((40 + 30 - 7) * 2 / 3)
+
+print (int((40 + 30 - 7) * 2 / 3))
+
+print ((float(40 + 30 - 7) * 2 / 3))
+
+
+
