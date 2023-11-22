ebd9cf0d6ce561b9b38112dbc3b789c8412193dd
ImanBanerjee <imanbanerjee03@gmail.com>
2023-11-22 18:10:41 +0530

    Minor improvements

--- tipcalculator.py
+++ tipcalculator.py
@@ -1,27 +1,32 @@
-# Get user input
-bill = input("Enter the bill amount: ")
-percentage = input("Enter the Tip %: ")
+# bill input
+bill = input("Enter the bill amount: $")
+
 
 # Remove dollar sign and leading/trailing whitespaces
-bill_ammount = bill.replace('$', '').strip() # strip () to remove railing whitespaces (learned from
+bill_amount = bill.replace('$', '').strip() # strip () to remove railing whitespaces (learned from
 # google
 
 
-# Remove percentage sign and leading/trailing whitespaces
-percent_ammount = percentage.replace('%', '').strip()
-
 
 # Convert the cleaned string to an integer
-ammount = int(bill_ammount)
+amount = float(bill_amount)
 
-percent = int(percent_ammount)
+print(f"some suggestions : \n15% - {amount*(15/100):.2f}\n17% - {amount*(17/100):.2f}\n18% - {amount*(18/100):.2f}")
+# bill input
+
+percentage = input("Enter the Tip amount: %")
+
+# Remove percentage sign and leading/trailing whitespaces
+percent_amount = percentage.replace('%', '').strip()
+
+percent = float(percent_amount)
 
 
-tip = int(ammount * (percent/100)) #tip ammount
+tip = float(amount * (percent/100)) #tip ammount
 
-total = int(ammount + tip) # total ammount with tip
+total = float(amount + tip) # total amount with tip
 
 # Print or return the result
-print(f"Tip Ammount is {tip} and Total ammount with tip is {total}")
+print(f"Tip Amount is ${tip:.2f}\nTotal amount with tip is ${total:.2f}")
 
 
