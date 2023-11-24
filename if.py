a8dd4e02283d938cbf183ff0f72f9bae72563ba6
ImanBanerjee <imanbanerjee03@gmail.com>
2023-11-24 15:15:43 +0530

    Minor improvements

---
+++ if.py
@@ -0,0 +1,8 @@
+answer = input("Type the password: ") # takses user input
+password = ("2023") # variable
+if answer == password: # if input is correct then print
+    print("Hello world!")
+
+if answer != password: # if input is incorrect then run
+    print("Incorrect password!")
+
